0e1bdba0f75e9ce33604d82c422569e58c2991a0
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-14 02:10:05 +0900

    line7

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
-$httpClient = new \LINE\LINEBot\httpClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
